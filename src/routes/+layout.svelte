075712ebd179bf196f7c4fc7c8c4aa6e7cfcaf58
ItsNeil17 <neil@willofsteel.me>
2025-11-21 15:36:01 +0530

    feat: remove custom site
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import "./layout.css";
-	
-	let { children } = $props();
-</script>
-
-<main class="fixed left-0 top-0 w-screen min-h-screen bg-zinc-800 text-gray-200 font-sans">
-	{@render children()}
-</main>
--- src/routes/docs/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { getArticleList } from "$lib/server/docs.ts";
-import { searchIndex } from "$lib/server/search.ts";
-
-export function load() {
-    return {
-        articleList: getArticleList(),
-        searchIndex: searchIndex
-    }
-}
